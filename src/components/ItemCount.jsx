6a9b45541fbf21d0e751ebd387f692382b86a572
JoacoML <joaco.martin.lanfranchi@gmail.com>
2022-07-30 16:41:30 +0200

    Fixed quantity in ItemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,33 +1,29 @@
-import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
 
 const ItemCount = ({initial, stock, onAdd}) => {
-    const [counter, setCounter] = useState(initial)
+    const [counter, setCounter] = useState(parseInt(initial))
 
     const add = () => {
-        if( counter < stock ){
-            const aux = counter+1
-            setCounter(aux)
-        }else {
-            console.log("No more stock")
-        }
+        setCounter(counter + 1)
     }
 
     const subtract = () => {
-        if( counter > initial ){
-            const aux = counter-1
-            setCounter(aux)
-        }
+        setCounter(counter - 1)
     }
+
+    useEffect(() => {
+        setCounter(parseInt(initial))
+    }, [initial])
 
     return (
         <div>
             <div className="">
-                <button className="btn btn-dark"onClick={subtract}>-</button>
+                <button className="btn btn-dark" disabled={counter <= 1} onClick={subtract}>-</button>
                 <p>{counter}</p>
-                <button className="btn btn-dark" onClick={add}>+</button>
+                <button className="btn btn-dark" disabled={counter >= stock } onClick={add}>+</button>
             </div>
-            <button className="btn btn-primary" onClick={onAdd}>Add to cart</button>
+            <button className="btn btn-primary" disabled={stock <= 0 } onClick={() => onAdd(counter)}>Add to cart</button>
         </div>
     )
 }
